2f9acad8b36c3d46366b35bb503e49b65aebb73c
TRex-Dino <69787885+TRex-Dino@users.noreply.github.com>
2021-06-02 21:27:38 +0300

    Add files via upload

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Dmitry on 02.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
